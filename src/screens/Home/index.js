c920ed42c8dfeb3eae1eb7376ce07428d80fbbce
William Roger <williamroger.frontend@gmail.com>
2020-09-08 01:05:20 -0300

    Finalizado barra de Sugestões

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -4,25 +4,36 @@ import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
 
 import {
   Wrapper,
+  Container,
   Header,
   BalanceContainer,
   BalanceTitle,
   Balance,
 } from "./styles";
 
+import Suggestions from '../../components/Suggestions';
+
 export default function Home() {
   return (
     <Wrapper>
-      <Header>
-        <MaterialCommunityIcons name="qrcode-scan" size={30} color="#10c86e" />
+      <Container>
+        <Header>
+          <MaterialCommunityIcons
+            name="qrcode-scan"
+            size={30}
+            color="#10c86e"
+          />
 
-        <BalanceContainer>
-          <BalanceTitle>Meu saldo</BalanceTitle>
-          <Balance>R$ 0,00</Balance>
-        </BalanceContainer>
+          <BalanceContainer>
+            <BalanceTitle>Meu saldo</BalanceTitle>
+            <Balance>R$ 0,00</Balance>
+          </BalanceContainer>
 
-        <AntDesign name="gift" size={30} color="#10c86e"/>
-      </Header>
+          <AntDesign name="gift" size={30} color="#10c86e" />
+        </Header>
+
+        <Suggestions />
+      </Container>
     </Wrapper>
   );
 }
