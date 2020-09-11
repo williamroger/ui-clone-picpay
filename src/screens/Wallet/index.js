224dc5f79914c83623db3feba813e56fa725b519
William Roger <williamroger.frontend@gmail.com>
2020-09-11 00:26:44 -0300

    Finalizado componente Header da Carteira

--- src/screens/Wallet/index.js
+++ src/screens/Wallet/index.js
@@ -1,7 +1,53 @@
 import React from 'react';
 
-import { Text } from 'react-native';
+import { Feather, MaterialCommunityIcons } from '@expo/vector-icons';
+
+import {
+  Wrapper,
+  Header,
+  HeaderContainer,
+  Title,
+  BalanceContainer,
+  Value,
+  Bold,
+  EyeButton,
+  Info,
+  Actions,
+  Action,
+  ActionLabel,
+} from "./styles";
 
 export default function Wallet() {
-  return <Text>Wallet</Text>;
+  return (
+    <Wrapper>
+      <Header colors={["#52e78c", "#1ab563"]}>
+        <HeaderContainer>
+          <Title>Saldo PicPay</Title>
+
+          <BalanceContainer>
+            <Value>
+              R$ <Bold>0,00</Bold>
+            </Value>
+            <EyeButton>
+              <Feather name="eye" size={28} color="#fff" />
+            </EyeButton>
+          </BalanceContainer>
+
+          <Info>Seu saldo está rendendo 100% do CDI</Info>
+
+          <Actions>
+            <Action>
+              <MaterialCommunityIcons name="cash" size={24} color="#fff" />
+              <ActionLabel>Adicionar</ActionLabel>
+            </Action>
+
+            <Action>
+              <MaterialCommunityIcons name="bank" size={20} color="#fff" />
+              <ActionLabel>Adicionar</ActionLabel>
+            </Action>
+          </Actions>
+        </HeaderContainer>
+      </Header>
+    </Wrapper>
+  );
 }
